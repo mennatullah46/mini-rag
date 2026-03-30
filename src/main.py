fd4c73428c0d9b383e1fc7268ba26ecdbe258a29
mennatullah46 <mmenna.463darwish@gmail.com>
2026-03-31 00:00:10 +0200

    setup vector db

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from contextlib import asynccontextmanager
 from helpers.config import get_settings
 from stores.llm.LLMProviderFactory import LLMProviderFactory
+from stores.vectordb.VectorDBProviderFactory import VectorDBProviderFactory
 
 
 
@@ -26,13 +27,18 @@ async def lifespan(app: FastAPI):
     app.embedding_client = llm_provider_factory.create(provider=settings.EMBEDDING_BACKEND)
     app.embedding_client.set_embedding_model(model_id=settings.EMBEDDING_MODEL_ID,
                                              embedding_size=settings.EMBEDDING_MODEL_SIZE)
-
+    
+    # vector db client
+    app.vectordb_client = VectorDBProviderFactory.create(
+        provider=settings.VECTOR_DB_BACKEND
+    )
+    app.vectordb_client.connect()
     
     yield  # application runs after this
 
     # --- shutdown ---
     app.mongo_conn.close()
-    print("MongoDB connection closed")
+    app.vectordb_client.disconnect()
 
 app = FastAPI(lifespan=lifespan)
 
